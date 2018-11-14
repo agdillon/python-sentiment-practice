7c7dd0d8ef845499d847a5253d53dacc067db009
Amanda Dillon <agdillon@gmail.com>
2018-11-14 12:01:31 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,10 @@
+import config
+
+api_key = config.api_key
+api_secret = config.api_secret
+access_token = config.access_token
+token_secret = config.token_secret
+
+import tweepy
+from datetime import datetime
+from textblob import TextBlob
