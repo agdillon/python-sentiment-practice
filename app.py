298e57f8843a2a47cee20764059cc923af58d9f7
Amanda Dillon <agdillon@gmail.com>
2018-11-15 09:28:42 -0700

    make dictionary of response data

--- app.py
+++ app.py
@@ -1,9 +1,4 @@
-# This app is partially based on the tutorial at
-# https://medium.freecodecamp.org/basic-data-analysis-on-twitter-with-python-251c2a85062e
-# Note: I had to modify tweepy code because of use of async keyword, see issue at
-# https://github.com/tweepy/tweepy/issues/1017
-
-# list of stuff I changed because it didn't make sense to me:
+# list of stuff I changed from tutorial because it didn't make sense to me:
 # from time import sleep --- unnecessary?  never used anywhere
 # analysis = TextBlob(tweet.text).sentiment --- they had an extra line for no reason
 # number += 1 --- they had number = number + 1, meh
@@ -19,33 +14,32 @@ import tweepy
 from datetime import datetime
 from textblob import TextBlob
 
+# auth and setup for Twitter API
 auth = tweepy.OAuthHandler(api_key, api_secret)
 auth.set_access_token(access_token, token_secret)
-
 api = tweepy.API(auth)
 
 # hardcode for now, want to get this data from form on frontend (post route)
 search_term = 'python'
 number_of_tweets = 50
 
-# polarity_list is a list of sentiment of different tweets, numbers_list is
-# just a corresponding list of numbers 1 through number_of_tweets, for graphing
-# purposes (which seems goofy, why not just make a range)
+# polarity_list is a list of sentiment polarity of each different tweet
 polarity_list = []
-numbers_list = []
-number = 1
 
 for tweet in tweepy.Cursor(api.search, search_term, lang='en').items(number_of_tweets):
     try:
         analysis = TextBlob(tweet.text).sentiment
         polarity = analysis.polarity
         polarity_list.append(polarity)
-        numbers_list.append(number)
-        number += 1
 
     except tweepy.TweepError as e:
         print(e.reason)
 
-# construct JSON response containing: number of tweets, polarity_list,
-# date and time of analysis, average polarity?
-# console.log it for now, then set up server layer
+timestamp = datetime.now().strftime('%c')
+
+response_dictionary = {}
+response_dictionary['number_of_tweets'] = number_of_tweets
+response_dictionary['polarity_list'] = polarity_list
+response_dictionary['timestamp'] = timestamp
+
+print(response_dictionary)
